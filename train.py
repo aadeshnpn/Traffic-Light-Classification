47c8276eb8794d232439c7040fb399f4ad57fcab
aadeshnpn <neupaneaadesh@gmail.com>
2020-11-29 12:25:24 +0000

    weird errors with pytorch fixed

--- train.py
+++ train.py
@@ -85,6 +85,7 @@ class BSTLDataset(Dataset):
     fname = 'bsltd_train.pkl' if train else 'bsltd_test.pkl'
     self.data = load_pickle(root, fname)
     self.train = train
+    print('datalenght', self.train, len(self.data))
     self.transform = get_transform(train)
 
   def __getitem__(self, index):
@@ -101,10 +102,14 @@ class BSTLDataset(Dataset):
         [EVAL_ID_MAP[SIMPLIFIED_CLASSES[t['label']]]])
     boxes = torch.as_tensor(boxes, dtype=torch.float32)
     labels = torch.as_tensor(labels, dtype=torch.int64)
+    labels = torch.reshape(labels, (labels.shape[0],))
+    # print(boxes.shape, labels.shape)
+    # if boxes.shape[0] ==1:
+    #  print(boxes, labels)
     return image, {'boxes': boxes, 'labels': labels}
 
   def __len__(self):
-    return len(self.data)
+    return 100 # len(self.data)
 
 
 def run_one_epoch(
@@ -150,22 +155,56 @@ def evaluate(model, test_loader, device):
 
 def train():
   import torchvision
+  from torchvision.models.detection import FasterRCNN
+  from torchvision.models.detection.rpn import AnchorGenerator
   # load a model pre-trained pre-trained on COCO
   from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
   model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
     pretrained=True)
   num_classes = 4  # 1 class (person) + background
   # get number of input features for the classifier
-  in_features = model.roi_heads.box_predictor.cls_score.in_features
+  # in_features = model.roi_heads.box_predictor.cls_score.in_features
   # replace the pre-trained head with a new one
-  model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+  # model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+  backbone = torchvision.models.mobilenet_v2(pretrained=True).features
+  # FasterRCNN needs to know the number of
+  # output channels in a backbone. For mobilenet_v2, it's 1280
+  # so we need to add it here
+  backbone.out_channels = 1280
+
+  # let's make the RPN generate 5 x 3 anchors per spatial
+  # location, with 5 different sizes and 3 different aspect
+  # ratios. We have a Tuple[Tuple[int]] because each feature
+  # map could potentially have different sizes and
+  # aspect ratios
+  anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
+                                    aspect_ratios=((0.5, 1.0, 2.0),))
+
+  # let's define what are the feature maps that we will
+  # use to perform the region of interest cropping, as well as
+  # the size of the crop after rescaling.
+  # if your backbone returns a Tensor, featmap_names is expected to
+  # be [0]. More generally, the backbone should return an
+  # OrderedDict[Tensor], and in featmap_names you can choose which
+  # feature maps to use.
+  roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0', '1', '2', '3'],
+                                                  output_size=7,
+                                                  sampling_ratio=2)
+
+  # put the pieces together inside a FasterRCNN model
+  model = FasterRCNN(backbone,
+                    num_classes=4,
+                    rpn_anchor_generator=anchor_generator,
+                    box_roi_pool=roi_pooler)
   # replace the pre-trained head with a new one
-  device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-  # print(device)
+  # device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+  device = 'cpu'
   # use our dataset and defined transformations
   dataset = BSTLDataset()
   dataset_test = BSTLDataset(train=False)
-
+  indices = torch.randperm(len(dataset)).tolist()
+  dataset = torch.utils.data.Subset(dataset, indices[:-50])
+  dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
   # define training and validation data loaders
   data_loader = torch.utils.data.DataLoader(
       dataset, batch_size=2, shuffle=True, num_workers=4,
@@ -189,9 +228,9 @@ def train():
                                                   gamma=0.1)
 
   # let's train it for 10 epochs
-  num_epochs = 10
+  num_epochs = 2
 
-  for epoch in range(num_epochs):
+  for epoch in tqdm(range(num_epochs)):
     # train for one epoch, printing every 10 iterations
     run_one_epoch(data_loader, optimizer, model, lr_scheduler, device)
     evaluate(model, data_loader_test, device=device)
